b8acefce974c634edde43092599f5b8868ffef6e
Hoang Anh <leanhhoang145@gmail.com>
2025-07-05 23:02:16 +0700

    Update thêm route tinhTuoi và hàm tinhTuoi()

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
+import datetime
 
 app = Flask(__name__)
 CORS(app)  # Cho phép truy cập từ frontend
@@ -11,6 +12,13 @@ def sum_numbers():
     a = data.get('a', 0)
     b = data.get('b', 0)
     return jsonify({'sum': a + b})
+
+@app.route('/tinhTuoi', medthods=['POST'])
+def tinhTuoi():
+    data = request.get_json()
+    namSinh = data.get('namSinh', 0)
+    tuoi = datetime.datetime.now().year - namSinh
+    return jsonify({'tuoi': tuoi})
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
